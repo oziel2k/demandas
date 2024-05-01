a8b12090d8ca3174c125dc493aace9e6ee4026f6
Oziel2k <Oziel2k@outlook.com>
2024-05-01 09:41:26 -0300

    Branch main Configurando o projeto
    	modified:   demandas2/.gitignore
    	modified:   demandas2/asgi.py
    	modified:   demandas2/wsgi.py
    	modified:   manage.py

--- demandas2/asgi.py
+++ demandas2/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demandas2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demandas2.settings.config')
 
 application = get_asgi_application()
--- demandas2/wsgi.py
+++ demandas2/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demandas2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demandas2.settings.config')
 
 application = get_wsgi_application()
